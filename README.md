708e2dceeb13baed416667cc019280d320ae898f
Thomas Capelle <tcapelle@protonmail.com>
2020-10-01 12:06:45 +0200

    updated Readme

--- README.md
+++ README.md
@@ -6,6 +6,15 @@ This file will become your README and also the index of your documentation.
 
 ## Install
 
-`pip install fastbench
+You should only need the latest fastai. Please create a clean new environment to launch the benchmarks.
+
+You can run this repo directly after cloning it.
 
 ## How to use
+
+you should run this a script (it uses fascript under the hood)
+
+```bash
+python bench.py --gpu 0
+```
+will run the benchs on the gpu 0. (default is running using `dataparallel` on all available GPUs)
---
+++ bench.py
@@ -0,0 +1,42 @@
+import torch
+from fastscript import *
+from fastbench.utils import show_install
+
+from fastbench.train_imagenette import main as image_main
+from fastbench.train_imdbclassifier import main as imdb_main
+from fastbench.train_wt2 import main as wt2_main
+
+
+"""Compute image benchmarks on imagenette"""
+
+welcome_str="""Welcome to fastbench! your friendly benchmark for GPU using fastai.\n"""
+
+
+
+
+@call_parse
+def main(
+    gpu:   Param("GPU to run on", int)=None,
+    epochs:Param("Number of epochs", int)=5,
+    runs:  Param("Number of times to repeat training", int)=1,
+):
+    print(welcome_str)
+    show_install()
+    print('==================')
+    if gpu is not None: 
+        print(f'\nRunning bench on: GPU: {torch.cuda.get_device_name(gpu)}')
+        
+    #run image benchsmarks
+    print('\nBenchmarking Imagenette')
+    image_main(gpu=gpu,epochs=epochs,fp16=0,bs=64,runs=runs)
+    image_main(gpu=gpu,epochs=epochs,fp16=1,bs=128,runs=runs)
+    
+    #run imdb
+    print('\nBenchmarking IMDB')
+    imdb_main(gpu=gpu,epochs=epochs,fp16=0,bs=64,runs=runs)
+    imdb_main(gpu=gpu,epochs=epochs,fp16=1,bs=128,runs=runs)
+    
+    #run tabular
+    print('\nBenchmarking Tabular')
+    imdb_main(gpu=gpu,epochs=epochs,fp16=0,runs=runs)
+    imdb_main(gpu=gpu,epochs=epochs,fp16=1,runs=runs)
